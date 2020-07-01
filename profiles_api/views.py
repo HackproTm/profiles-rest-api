cd2013fc134ab5bc781c3f782e0fdc7ab5d96665
Edwin Mantilla <emantillas@psl.com.co>
2020-07-01 10:33:01 -0500

    Added user profile feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
@@ -65,7 +66,8 @@ class HelloViewSet(viewsets.ViewSet):
         """Return a hello message"""
 
         a_viewset = [
-            'Uses actions (list, create, retrieve, update, partial_update, destroy)',
+            'Uses actions (list, create, retrieve, update, ',
+            'partial_update, destroy)',
             'Automatically maps to URLs using Routers',
             'Provides more functionality with less code',
         ]
@@ -117,3 +119,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
